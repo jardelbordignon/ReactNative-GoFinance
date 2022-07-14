fc605d0927824cd845379d51ce4a3d354e87e83a
Jardel Bordignon <jardel1101@gmail.com>
2022-07-14 20:52:28 -0300

    feat: usando styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import { Center, NativeBaseProvider } from 'native-base'
 import React from 'react'
 
 import { Dashboard } from './screens/Dashboard'
-import { theme } from './styles/theme'
-
-const ThemedContent = () => (
-  <Center
-    flex={1}
-    _light={{ bg: 'app.bg_light' }}
-    _dark={{ bg: 'app.bg_dark' }}>
-    <Dashboard />
-  </Center>
-)
 
 const App = () => {
-  return (
-    <NativeBaseProvider theme={theme}>
-      <ThemedContent />
-    </NativeBaseProvider>
-  )
+  return <Dashboard />
 }
 
 export default App
