1efeb0836cead3a47c1a8d55e7818ed258cdf9b6
Jardel Bordignon <jardel1101@gmail.com>
2022-07-14 23:25:55 -0300

    feat: estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components/native'
 
 import { Dashboard } from './screens/Dashboard'
+import { theme } from './styles/theme'
 
 const App = () => {
-  return <Dashboard />
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#5636D3',
+    secondary: '#FF872C',
+    secondary_light: '#FF872C30',
+    success: '#12A454',
+    success_light: '#12A45450',
+    attention: '#E83F5B',
+    attention_ligth: '#E83F5B50',
+    shape: '#FFF',
+    title: '#363F5F',
+    text: '#969CB2',
+    background: '#F0F2F5',
+  },
+}
