0ad295c292f06ceb871a6330ea445ab60ee07d5d
Jardel Bordignon <jardel1101@gmail.com>
2022-08-12 01:00:34 -0300

    Estilizando a Tab Navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
+import { Platform } from 'react-native'
+import Feather from 'react-native-vector-icons/Feather'
+import { useTheme } from 'styled-components'
 
 import { Dashboard } from 'src/screens/Dashboard'
 import { Register } from 'src/screens/Register'
@@ -7,11 +10,47 @@ import { Register } from 'src/screens/Register'
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function AppRoutes() {
+  const { colors } = useTheme()
+
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.secondary,
+        tabBarInactiveTintColor: colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 80,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        },
+      }}>
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="dollar-sign" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   )
 }
